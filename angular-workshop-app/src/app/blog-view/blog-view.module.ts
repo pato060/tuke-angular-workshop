0933449a27d1272a0d42814acff269e5bd06bab3
Patrik Urda <urda.p060@gmail.com>
2022-04-21 16:50:32 +0200

    little changes

--- angular-workshop-app/src/app/blog-view/blog-view.module.ts
+++ angular-workshop-app/src/app/blog-view/blog-view.module.ts
@@ -7,6 +7,8 @@ import { BlogViewComponent } from './blog-view.component';
 import { MatCardModule } from '@angular/material/card';
 import { BlogPostComponent } from './blog-post/blog-post.component';
 import { BlogGridComponent } from './blog-grid/blog-grid.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,12 @@ import { BlogGridComponent } from './blog-grid/blog-grid.component';
     BlogPostDetailComponent,
     BlogGridComponent,
   ],
-  imports: [CommonModule, BlogViewRoutingModule, MatCardModule],
+  imports: [
+    CommonModule,
+    BlogViewRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class BlogViewModule {}
